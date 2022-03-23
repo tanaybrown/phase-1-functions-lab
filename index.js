542703454e12cb8a1bc2f4cafd385b6df7768391
Tanya <tanaybrown@gmail.com>
2022-03-22 17:57:19 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(streetNumber) {
+    return Math.abs(42 - streetNumber)
+}
+
+function distanceFromHqInFeet(streetNumber) {
+    return distanceFromHqInBlocks(streetNumber)*264
+}
+
+function distanceTravelledInFeet(destination, start) {
+    return Math.abs(destination - start) * 264
+ }
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet (start, destination)
+    if (distanceInFeet < 400) {
+        return 0
+    }   else if (distanceInFeet >= 400 && distanceInFeet <=2000) {
+        const totalDistanceInFeet2 = distanceInFeet - 400
+        return totalDistanceInFeet2 * .02
+    }   else if (distanceInFeet > 2000 && distanceInFeet <=2500) {
+        return 25
+    }   else {
+        return "cannot travel that far"
+    }
+}
+
